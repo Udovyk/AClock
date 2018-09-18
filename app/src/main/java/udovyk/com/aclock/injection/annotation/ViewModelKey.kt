24f655a0361cd50475b261723e3d1c8a288f38c7
udovik.s <matrix.has.you.13@gmail.com>
2018-09-18 17:40:47 +0300

    drop .idea folder, fix support lib version

---
+++ app/src/main/java/udovyk/com/aclock/injection/annotation/ViewModelKey.kt
@@ -0,0 +1,16 @@
+package udovyk.com.aclock.injection.annotation
+
+import android.arch.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+
+@MustBeDocumented
+@Target(
+        AnnotationTarget.FUNCTION,
+        AnnotationTarget.PROPERTY_GETTER,
+        AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
---
+++ app/src/main/java/udovyk/com/aclock/injection/scope/ActivityScope.kt
@@ -0,0 +1,7 @@
+package udovyk.com.aclock.injection.scope
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
